d1f0fee0bf535877de668503fb83d08a63a9c0c3
Andy Dirnberger <andy.dirnberger@gmail.com>
2020-03-18 01:36:21 -0400

    Add fish function to delete merged Git branches
    This function will offer to delete any Git branches that have been
    merged into `origin/master`. `git-prune` is already a command, but Git
    can't find fish functions so this won't shadow it.

---
+++ .config/fish/functions/git-prune.fish
@@ -0,0 +1,42 @@
+# This is derived from https://gist.github.com/robmiller/5133264. It has been
+# adapted to only delete local branches (since I have git prune remote branches
+# by default) and to echo the command to restore each branch in case one is
+# needed again for some reason.
+
+function git-prune
+    set -l CURRENT_BRANCH (git rev-parse --abbrev-ref HEAD)
+    if test "$CURRENT_BRANCH" != "master"
+        echo "git-prune must be run from master"
+        return 1
+    end
+
+    git fetch
+
+    set -l MERGED (git branch --merged origin/master | grep -v 'master$' | string trim)
+    if test -z "$MERGED"
+        echo "There are no branches that have been merged."
+        return
+    end
+
+    echo "The following branches have been merged and will be removed:"
+    echo
+    for branch in $MERGED
+        echo \t$branch
+    end
+    echo
+    read --local --prompt-str "Continue [yN]? " REPLY
+    if test (string lower "$REPLY") != "y"
+        return
+    end
+
+    echo "Your branches are being deleted. You can restore them with:"
+    echo
+    for branch in $MERGED
+        set -l HASH (git rev-parse $branch)
+        echo \tgit checkout -b $HASH $branch
+        git branch --quiet --delete $branch
+    end
+    echo
+
+    echo "done"
+end
