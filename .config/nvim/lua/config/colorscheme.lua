492eee80a9272318d4be188f49e72b1c012409a1
Andy Dirnberger <andy.dirnberger@gmail.com>
2021-10-21 16:27:35 -0400

    [Neovim] Clean up unused plugins
    Everything here has been commented out. I can always get them back from
    the version history.

--- .config/nvim/lua/config/colorscheme.lua
+++ .config/nvim/lua/config/colorscheme.lua
@@ -1,13 +1,10 @@
 local fn = vim.fn
 
 vim.g.gruvbox_material_background = "soft"
--- vim.g.solarized_italics = false
 
 vim.opt.termguicolors = true
 
 vim.cmd([[ colorscheme gruvbox-material ]])
--- vim.cmd([[ colorscheme solarized-flat ]])
--- vim.cmd [[ colorscheme solarized8 ]]
 
 if fn.has("mac") > 0 then
   vim.opt.background = "dark"
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -21,7 +21,6 @@ return require("packer").startup(function(use)
 
   -- Colorscheme
   use({
-    -- "https://github.com/ishan9299/nvim-solarized-lua",
     "https://github.com/sainnhe/gruvbox-material",
     config = [[ require('config.colorscheme') ]],
   })
@@ -145,8 +144,6 @@ return require("packer").startup(function(use)
     "https://github.com/dhruvasagar/vim-prosession",
     requires = { "https://github.com/tpope/vim-obsession" },
   })
-
-  -- use 'https://github.com/Vimjas/vim-python-pep8-indent'
 
   use("https://github.com/raimon49/requirements.txt.vim")
 
