e00917b093b57f035c12168ec82dcdcc67d23738
Andy Dirnberger <andy.dirnberger@gmail.com>
2021-11-30 16:59:58 -0500

    [Neovim] Upgrade to 0.6.0
    While the upgrade itself is handled implicitly by running `brew bundle`,
    this removes the 0.5-compatibility branches for plugins.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -33,11 +33,6 @@ return require("packer").startup(function(use)
     "https://github.com/neovim/nvim-lspconfig",
     {
       "https://github.com/tami5/lspsaga.nvim",
-      -- master has dropped support for Neovim 0.5 in preparation of the release
-      -- of 0.6. Until 0.6 has a stable release, pin the version to one that's
-      -- compatible with 0.5.
-      -- TODO: Remove this once 0.6 has been released.
-      branch = "nvim51",
       config = function()
         require("config.lspsaga")
       end,
@@ -52,8 +47,6 @@ return require("packer").startup(function(use)
       "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
       "https://github.com/nvim-treesitter/playground",
     },
-    -- TODO: Remove this once 0.6 has been released.
-    branch = "0.5-compat",
     run = ":TSUpdate",
     config = function()
       require("config.treesitter")
