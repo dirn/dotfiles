2f7e6b76e85b72394ee45db049997bd1363d309a
Andy Dirnberger <andy.dirnberger@gmail.com>
2021-12-01 16:53:17 -0500

    [Neovim] Remove outdated highlight settings
    These highlight groups seem to have stopped working long ago. No sense
    in keeping them around.

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -24,10 +24,3 @@ require("gitsigns").setup({
     enable = true,
   },
 })
-
--- gitsigns reverses the fg and bg colors and uses the background of the
--- number column rather than the background of the sign column. Fix that.
--- TODO: Is there a way to get the background from SignColumn?
-vim.cmd([[ highlight GitSignsAdd gui=NONE guibg=NONE ctermbg=242 ]])
-vim.cmd([[ highlight GitSignsChange gui=NONE guibg=NONE ctermbg=242 ]])
-vim.cmd([[ highlight GitSignsDelete gui=NONE guibg=NONE ctermbg=242 ]])
