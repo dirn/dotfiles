8871dcdaa58983b3a68d198b41656b6ca2703d5a
Andy Dirnberger <andy.dirnberger@gmail.com>
2022-12-29 20:57:43 -0500

    [Neovim] Lazy load fzf.vim

--- .config/nvim/after/plugin/fzf.lua
+++
@@ -1,6 +0,0 @@
-local options = { noremap = true, silent = true }
-
--- Fuzzy find files.
-vim.keymap.set("n", "<c-p>", vim.cmd.GitFiles, options)
-vim.keymap.set("n", "<leader>rg", vim.cmd.Rg, options)
-vim.keymap.set("n", "<leader>ls", vim.cmd.Buffers, options)
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -132,6 +132,11 @@ local plugins = {
 
   {
     "https://github.com/junegunn/fzf.vim",
+    keys = {
+      { "<c-p>", vim.cmd.GitFiles, noremap = true, silent = true },
+      { "<leader>rg", vim.cmd.Rg, noremap = true, silent = true },
+      { "<leader>ls", vim.cmd.Buffers, noremap = true, silent = true },
+    },
     dependencies = { "https://github.com/junegunn/fzf" },
   },
 
