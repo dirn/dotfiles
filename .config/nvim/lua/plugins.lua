e55b378aa92646269d8f423a098c2c78b2238f99
Andy Dirnberger <andy.dirnberger@gmail.com>
2022-12-29 20:53:15 -0500

    [Neovim] Lazy load nvim-cmp

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -53,6 +53,7 @@ local plugins = {
 
   {
     "https://github.com/hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
       "https://github.com/hrsh7th/cmp-buffer",
       "https://github.com/hrsh7th/cmp-calc",
@@ -62,6 +63,45 @@ local plugins = {
       "https://github.com/hrsh7th/cmp-path",
       "https://github.com/nvim-lua/plenary.nvim",
     },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        formatting = {
+          format = function(entry, vim_item)
+            -- Show the source of the completion.
+            vim_item.menu = ({
+              buffer = "[Buffer]",
+              calc = "[Calc]",
+              cmp_git = "[Git]",
+              nvim_lsp = "[LSP]",
+              nvim_lua = "[Lua]",
+              path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<c-space>"] = cmp.mapping.complete(),
+          ["<c-e>"] = cmp.mapping.close(),
+          ["<cr>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "nvim_lua" },
+          { name = "buffer" },
+          { name = "cmp_git" },
+          { name = "path" },
+          { name = "calc" },
+        },
+      })
+
+      require("cmp_git").setup({
+        filetypes = { "gitcommit" },
+      })
+    end,
   },
 
   {
