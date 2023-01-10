b2c1c5c2862cb5bba760edb6808b6fbc4ffed3b6
Andy Dirnberger <andy.dirnberger@gmail.com>
2023-01-10 10:16:14 -0500

    [Neovim] Reload modified buffer on FocusGained

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -13,7 +13,10 @@ vim.opt.smarttab = true
 -- will reject the commit again or, even worse, I'll end up with merge
 -- conflicts. This will automatically reload it when something else changes it.
 vim.opt.autoread = true
-vim.api.nvim_create_autocmd("BufEnter", { command = "checktime" })
+vim.api.nvim_create_autocmd(
+  { "BufEnter", "FocusGained" },
+  { command = "checktime" }
+)
 
 -- Don't redraw the screen while executing macros (and other things).
 vim.opt.lazyredraw = true
