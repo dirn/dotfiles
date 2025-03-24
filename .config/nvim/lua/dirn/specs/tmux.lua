b8e51631d7d4aec30a57ecf828fcd1e5ab4165f0
Andy Dirnberger <andy.dirnberger@gmail.com>
2025-03-23 22:43:54 -0400

    [Neovim] Properly load tmux.nvim conditionally
    I should have been using `cond` all along.

--- .config/nvim/lua/dirn/specs/tmux.lua
+++ .config/nvim/lua/dirn/specs/tmux.lua
@@ -1,14 +1,13 @@
-if vim.fn.executable("tmux") > 0 then
-  return {
-    "https://github.com/aserowy/tmux.nvim",
-    opts = {
-      navigation = {
-        cycle_navigation = false,
-        enable_default_keybindings = true,
-        persist_zoom = true,
-      },
+return {
+  "https://github.com/aserowy/tmux.nvim",
+  cond = function()
+    return vim.fn.executable("tmux") > 0
+  end,
+  opts = {
+    navigation = {
+      cycle_navigation = false,
+      enable_default_keybindings = true,
+      persist_zoom = true,
     },
-  }
-else
-  return {}
-end
+  },
+}
