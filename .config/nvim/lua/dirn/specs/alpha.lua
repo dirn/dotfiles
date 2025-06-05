b2db49ed69d2bdc462f90aa871457a66276b2932
Andy Dirnberger <andy.dirnberger@gmail.com>
2025-06-05 15:52:27 -0400

    [Neovim] Add keybinding to show dashboard

--- .config/nvim/lua/dirn/specs/alpha.lua
+++ .config/nvim/lua/dirn/specs/alpha.lua
@@ -1,6 +1,15 @@
 return {
   "https://github.com/goolord/alpha-nvim",
   config = function()
-    require("alpha").setup(require("dirn.dashboard").config)
+    local alpha = require("alpha")
+    local config = require("dirn.dashboard").config
+
+    alpha.setup(config)
+
+    vim.keymap.set("n", "<leader>d", function()
+      alpha.start(false, config)
+    end, {
+      desc = "Show the dashboard.",
+    })
   end,
 }
