b39cb64e435c42066252bfb9efb7890e7e766629
Andy Dirnberger <andy.dirnberger@gmail.com>
2025-06-05 22:47:23 -0400

    [Neovim] Use recommended setup for mason-lspconfig
    When trying to sync my Neovim plugins with the Lazy lock file I saw
    
        mason.nvim has not been set up. Make sure to set up mason.nvim before mason-lspconfig.nvim. See :h mason-lspconfig-quickstart⏎
    
    This updates the spec for mason-lspconfig to match the recommended one
    in its README.

--- .config/nvim/lua/dirn/specs/mason-lspconfig.lua
+++ .config/nvim/lua/dirn/specs/mason-lspconfig.lua
@@ -1,5 +1,8 @@
--- Dependencies
--- nvim-lspconfig.nvim
--- mason.nvim
-
-return { "https://github.com/mason-org/mason-lspconfig.nvim", opts = {} }
+return {
+  "https://github.com/mason-org/mason-lspconfig.nvim",
+  opts = {},
+  dependencies = {
+    { "https://github.com/mason-org/mason.nvim", opts = {} },
+    "https://github.com/neovim/nvim-lspconfig",
+  },
+}
