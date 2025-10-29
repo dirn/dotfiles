02243fdc8b86dcae7883def060d9c82c45ce50ff
Andy Dirnberger <andy.dirnberger@gmail.com>
2025-10-29 11:51:58 -0400

    [Neovim] Turn on most ruff linters
    Rather than selectively enabling ruff linters, this takes the opposite
    approach: all linters will be enabled and I'll selectively disable the
    ones I don't want. Some of the ones being disabled seem useful, but they
    can be overly aggressive.

--- .config/nvim/after/lsp/ruff.lua
+++ .config/nvim/after/lsp/ruff.lua
@@ -2,7 +2,18 @@ return {
   init_options = {
     settings = {
       lint = {
-        extendSelect = { "ASYNC", "B", "F", "I", "N" },
+        extendSelect = { "ALL" },
+        ignore = {
+          "C90", -- not all functions can be simplified
+          "D1", -- I work with too much code that doesn't have docstrings
+          "DTZ", -- flake8-datetimez doesn't like naive date(time)s
+          "E501", -- my code is autoformatted; if the line is too long there's a reason
+          "ERA", -- flake8-eradicate seems to have a lot of false positives
+          "FIX", -- flake8-fixme wants me to fix everything
+          "PL", -- I'm not a fan of pylint
+          "TD", -- flake8-todos doesn't like too many TODOs in code I don't own
+        },
+        fixable = "ALL",
       },
     },
   },
