f0a58669a5031037bdf9a9f3fae25f572ee73746
Andy Dirnberger <andy.dirnberger@gmail.com>
2025-11-04 22:27:57 -0500

    [Neovim] Stop using diagnosticls for Python

--- .config/nvim/after/lsp/diagnosticls.lua
+++ .config/nvim/after/lsp/diagnosticls.lua
@@ -1,13 +1,9 @@
 return {
-  filetypes = { "fish", "lua", "python", "yaml" },
+  filetypes = { "fish", "lua", "yaml" },
   init_options = {
     filetypes = {
       fish = { "fish" },
-      python = { "mypy" },
       yaml = { "yamllint" },
-    },
-    formatFiletypes = {
-      python = "", -- Don't try to format Python files.
     },
   },
 }
